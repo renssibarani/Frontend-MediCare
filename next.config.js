15744be5b986b440734955d9344b57e03f0c4a30
Rens Junior Sibarani <garoga.sehat@gmail.com>
2023-05-31 09:34:47 +0700

    port

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  port: "3030",
   env: {
     user: {},
     token: null,
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const router = useRouter();
   const host = "http://localhost";
-  const port = 3000;
+  const port = 3030;
   env.url = `${host}:${port}`;
   env.user = {};
   env.data_user = {};
